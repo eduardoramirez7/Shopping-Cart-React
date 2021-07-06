e4a478fe5853f5d09828ca5406af610630f90aff
Luisa Ibagon <jeduard@unicauca.edu.co>
2021-07-05 19:28:05 -0500

    Implementacion Parte 1, incluye datos iniciales falsos y primera vista funcional, ademas se incluye la aplicacion del concepto de Drag and Drop.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers'
 import './index.css';
-import App from './App';
+import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
+let store = createStore(reducer)
+render(
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
